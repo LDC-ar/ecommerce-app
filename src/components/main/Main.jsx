d095872ebf6df6fb610b20d53cff2c577e172a79
LeandroDC <leandrocastagno@hotmail.com>
2022-09-17 20:42:44 -0300

    Hero iPhone maquetado

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ButtonBuy from "./ButtonBuy";
 import DesktopHeroHome from "./DesktopHeroHome";
+import DesktopHeroProductIphone from "./DesktopHeroProductIphone";
 import "./Main.css";
 
 function Main(props) {
 	return (
 		<div className="container-main">
 			<DesktopHeroHome />
+			<DesktopHeroProductIphone />
 
 			{props.children}
 			<ButtonBuy>{}</ButtonBuy>
