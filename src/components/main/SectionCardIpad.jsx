2e4987089a37d25234c543fdc99b1e2e06dac49b
LeandroDC <leandrocastagno@hotmail.com>
2022-09-23 19:13:13 -0300

    Arreglos en el codigo de componentes: SectionCards - ImageSlider - data

--- src/components/main/SectionCardIpad.jsx
+++ src/components/main/SectionCardIpad.jsx
@@ -3,24 +3,14 @@ import "./SectionCard.css";
 import { sectionTitle, cardsInfoIpad } from "../helpers/data";
 import Cards from "./Cards";
 
-
 const SectionCardIpad = () => {
 	return (
 		<div className="container-SectionCard">
 			<h3>{sectionTitle.title3}</h3>
 			<div className="cards">
-				{cardsInfoIpad.map((card)=>{
-					return (
-						<div key={card.id}>
-							<Cards
-							src={card.img}
-							title={card.title}
-							condition={card.condition}
-							description={card.description}
-							price={card.price}
-							/>
-						 </div>
-					)})}			
+				{cardsInfoIpad.map(card => {
+					return <Cards key={card.id} src={card.img} title={card.title} condition={card.condition} description={card.description} price={card.price} />;
+				})}
 			</div>
 		</div>
 	);
