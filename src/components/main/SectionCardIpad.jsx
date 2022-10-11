0680322147fdb8716af98e2d26c383432a10a9c0
LDC-ar <leandrocastagno@hotmail.com>
2022-10-11 20:26:08 -0300

    Fix - Carrousel productos funcionando 100%

--- src/components/main/SectionCardIpad.jsx
+++ src/components/main/SectionCardIpad.jsx
@@ -1,30 +1,54 @@
-import React from "react";
-import "./SectionCard.css";
+import React, { useState, useEffect } from "react";
 import { sectionTitle, productsInfo } from "../helpers/data";
 import Cards from "./Cards";
+import LeftArrow from "../images/left-arrow2.png";
+import RightArrow from "../images/right-arrow2.png";
+import "./SectionCard.css";
 
 const SectionCardIpad = ({ windowWidth, breakPoint }) => {
+	// Estado que guarda el array de productos con la categoria "Phone"
+	const [ipadArray, setIpadArray] = useState([]);
+
+	// Este efecto filtra y guarda todos los productos del array general de productos que pertenezcan a la categoria "Phone"
+	useEffect(() => {
+		setIpadArray(productsInfo.filter(product => product.category === "Ipad"));
+	}, []);
+
+	// Estado para slider en version mobile
+	const [current, setCurrent] = useState(0);
+	const length = ipadArray.length;
+
+	// Funciones de botones para mover las slides.
+	const nextSlide = () => {
+		setCurrent(current === length - 1 ? 0 : current + 1);
+	};
+
+	const prevSlide = () => {
+		setCurrent(current === 0 ? length - 1 : current - 1);
+	};
+
 	return (
 		<div className="container-SectionCard">
-			<h3>{sectionTitle.title3}</h3>
+			<h3>{sectionTitle.title1}</h3>
 			{/* Asi chequeas la condicion y renderiza un componente u otro*/}
 			{windowWidth < breakPoint ? (
-				<Cards
-					id={productsInfo[9].id}
-					category={productsInfo[9].category}
-					src={productsInfo[9].img}
-					title={productsInfo[9].title}
-					condition={productsInfo[9].condition}
-					description={productsInfo[9].description}
-					price={productsInfo[9].price}
-				/>
+				<div>
+					{ipadArray.map(
+						(card, index) =>
+							index === current && (
+								<Cards key={card.id} id={card.id} category={card.category} src={card.img} title={card.title} condition={card.condition} description={card.description} price={card.price} />
+							)
+					)}
+					<div className="product-carrousel-arrows">
+						<img src={LeftArrow} alt="Switch to left arrow" className="left-arrow" onClick={prevSlide} />
+						<img src={RightArrow} alt="Switch to right arrow" className="right-arrow" onClick={nextSlide} />
+					</div>
+				</div>
 			) : (
 				<div className="cards">
-					{productsInfo.map(card => {
-						if (card.category === "Ipad") {
-							return <Cards key={card.id} category={card.category} id={card.id} src={card.img} title={card.title} condition={card.condition} description={card.description} price={card.price} />;
-						}
-					})}
+					{ipadArray.map(card => (
+						<Cards key={card.id} id={card.id} category={card.category} src={card.img} title={card.title} condition={card.condition} description={card.description} price={card.price} />
+					))}
 				</div>
 			)}
 		</div>
--- src/components/main/SectionCardIphone.jsx
+++ src/components/main/SectionCardIphone.jsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { sectionTitle, productsInfo, cardsInfoIphone } from "../helpers/data";
+import { sectionTitle, productsInfo } from "../helpers/data";
 import Cards from "./Cards";
 import LeftArrow from "../images/left-arrow2.png";
 import RightArrow from "../images/right-arrow2.png";
 import "./SectionCard.css";
 
 const SectionCardIphone = ({ windowWidth, breakPoint }) => {
+	// Estado que guarda el array de productos con la categoria "Phone"
+	const [phoneArray, setPhoneArray] = useState([]);
+
+	// Este efecto filtra y guarda todos los productos del array general de productos que pertenezcan a la categoria "Phone"
+	useEffect(() => {
+		setPhoneArray(productsInfo.filter(product => product.category === "Phone"));
+	}, []);
+
+	// Estado para slider en version mobile
 	const [current, setCurrent] = useState(0);
-	const length = cardsInfoIphone.length;
+	const length = phoneArray.length;
 
 	// Funciones de botones para mover las slides.
 	const nextSlide = () => {
@@ -24,13 +33,12 @@ const SectionCardIphone = ({ windowWidth, breakPoint }) => {
 			{/* Asi chequeas la condicion y renderiza un componente u otro*/}
 			{windowWidth < breakPoint ? (
 				<div>
-					{cardsInfoIphone.map((card, index) => {
-						return (
-							<div key={index}>
-								{index === current && <Cards id={card.id} category={card.category} src={card.img} title={card.title} condition={card.condition} description={card.description} price={card.price} />}
-							</div>
-						);
-					})}
+					{phoneArray.map(
+						(card, index) =>
+							index === current && (
+								<Cards key={card.id} id={card.id} category={card.category} src={card.img} title={card.title} condition={card.condition} description={card.description} price={card.price} />
+							)
+					)}
 					<div className="product-carrousel-arrows">
 						<img src={LeftArrow} alt="Switch to left arrow" className="left-arrow" onClick={prevSlide} />
 						<img src={RightArrow} alt="Switch to right arrow" className="right-arrow" onClick={nextSlide} />
@@ -38,11 +46,9 @@ const SectionCardIphone = ({ windowWidth, breakPoint }) => {
 				</div>
 			) : (
 				<div className="cards">
-					{productsInfo.map(card => {
-						if (card.category === "Phone") {
-							return <Cards key={card.id} id={card.id} category={card.category} src={card.img} title={card.title} condition={card.condition} description={card.description} price={card.price} />;
-						}
-					})}
+					{phoneArray.map(card => (
+						<Cards key={card.id} id={card.id} category={card.category} src={card.img} title={card.title} condition={card.condition} description={card.description} price={card.price} />
+					))}
 				</div>
 			)}
 		</div>
